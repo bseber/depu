aabbd0c48191c28bf2ce81ca51339d905fd9f3b8
Benjamin Seber <seber@synyx.de>
2018-03-17 11:58:23 +0100

    added listr to improve task logging on cli

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const os = require("os");
 const hasFlag = require("has-flag");
 const parseArgs = require("minimist");
 const columnify = require("columnify");
+const Listr = require("listr");
 const logger = require("./logger");
 const {
   doCommit,
@@ -23,24 +24,45 @@ const config = {
   prefix: args.prefix || "",
 };
 
-async function updateDependencies() {
-  const data = await getOutdated();
-  const { dependencies, devDependencies } = await getUpdateable(data, config);
-  await doUpdate(dependencies, devDependencies);
-  if (dependencies.length !== 0 || devDependencies.length !== 0) {
-    await doCommit();
-    return Promise.resolve([...dependencies, ...devDependencies]);
-  }
-}
+const tasks = new Listr([
+  {
+    title: "get outdated dependencies",
+    task: async ctx => {
+      ctx.outdated = await getOutdated();
+    },
+  },
+  {
+    title: `get latest ${mode} versions`,
+    skip: ctx => !hasOutdated(ctx),
+    task: async ctx => {
+      ctx.updateable = await getUpdateable(ctx.outdated, config);
+    },
+  },
+  {
+    title: "update dependencies",
+    skip: ctx => !hasUpdateable(ctx),
+    task: async ctx => {
+      const { dependencies, devDependencies } = ctx.updateable;
+      await doUpdate(dependencies, devDependencies);
+      ctx.updated = [...dependencies, ...devDependencies];
+    },
+  },
+  {
+    title: "commit changes",
+    skip: ctx => !hasUpdated(ctx),
+    task: async () => {
+      await doCommit();
+    },
+  },
+]);
 
-updateDependencies(config)
-  .then(dependencies => {
-    if (dependencies.length === 0) {
-      logger.info("dependencies are up to date 😎");
-    } else {
-      logger.info(`updated ${dependencies.length} dependencies 💪`);
-      logger.info(LINE_BREAK);
-      const data = dependencies.reduce(
+tasks
+  .run()
+  .then(ctx => {
+    if (hasUpdated(ctx)) {
+      const updated = ctx.updated;
+      logger.info(`updated ${updated.length} dependencies 💪${LINE_BREAK}`);
+      const data = updated.reduce(
         (data, dep) => [
           ...data,
           { name: dep.moduleName, from: dep.current, to: dep.install },
@@ -48,6 +70,30 @@ updateDependencies(config)
         [],
       );
       logger.info(columnify(data));
+    } else {
+      logger.info("dependencies are up to date 😎");
     }
   })
-  .catch(error => logger.error("Whoops... 😱", error));
+  .catch(error => logger.info("Whoops... 😱", error));
+
+function hasOutdated(ctx) {
+  return !empty(ctx.outdated);
+}
+
+function hasUpdateable(ctx) {
+  if (!hasOutdated(ctx)) {
+    return false;
+  }
+  return (
+    !empty(ctx.updateable.dependencies) ||
+    !empty(ctx.updateable.devDependencies)
+  );
+}
+
+function hasUpdated(ctx) {
+  return !empty(ctx.updated);
+}
+
+function empty(list) {
+  return !list || list.length === 0;
+}
--- src/shell-exec.js
+++ src/shell-exec.js
@@ -1,9 +1,7 @@
 const childProcess = require("child_process");
-const logger = require("./logger");
 
 module.exports = async function exec(command) {
   return new Promise((resolve, reject) => {
-    logger.info("exec: ", command);
     childProcess.exec(command, (error, stdout, stderr) => {
       if (error) {
         reject({ error, stdout, stderr });
